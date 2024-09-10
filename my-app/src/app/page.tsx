a59b856fb9a6b67e41ccf833a0cf92f7464bcf3f
shin72mican <shin72mican@gmail.com>
2024-09-10 23:44:07 +0900

    next practice

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import type { Photo } from "@/type";
 
-export default function Page() {
+async function getPhotos() {
+  const data: { photos: Photo[] } = await fetch(
+    "http://localhost:8080/api/photos"
+  ).then((res) => res.json());
+  return data.photos.map(({ id, title }) => ({ id, title }));
+}
+
+export default async function Page() {
+  const photos = await getPhotos();
   return (
     <div>
       <h1>トップ画面</h1>
       <ul>
-        <li>
+        {/* <li>
           <Link href={`/photos/1`}>写真1</Link>
         </li>
         <li>
@@ -13,7 +22,12 @@ export default function Page() {
         </li>
         <li>
           <Link href={`/photos/2`}>写真2</Link>
-        </li>
+        </li> */}
+        {photos.map(({ id, title }) => (
+          <li key={id}>
+            <Link href={`/photos/${id}`}>{title}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ my-app/src/type.ts
@@ -0,0 +1,8 @@
+export type Photo = {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  authorId: string;
+  categoryId: string;
+}
